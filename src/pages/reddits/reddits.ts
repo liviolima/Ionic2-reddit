15dbbb1cfa0a005a2a33eff266aa3f9cb47840db
LivioLima <livio.lima.br@gmail.com>
2017-01-21 10:09:35 -0300

    Add details page to show all information of an item

--- src/pages/reddits/reddits.ts
+++ src/pages/reddits/reddits.ts
@@ -4,6 +4,8 @@ import { NavController } from 'ionic-angular';
 
 import {RedditService} from '../../app/services/reddit.service';
 
+import {DetailsPage} from '../details/details';
+
 @Component({
   selector: 'reddits',
   templateUrl: 'reddits.html'
@@ -15,12 +17,18 @@ export class RedditsPage {
   }
 
   ngOnInit(){
-  	this.getPosts('sports', 5);
+  	this.getPosts('science', 10);
   }
 
   getPosts(category, limit){
   	this.redditService.getPosts(category, limit).subscribe(response => {
   		this.items = response.data.children;
   	});
+  }
+
+  viewItem(item){
+    this.navCtrl.push(DetailsPage,{
+      item:item
+    })
   }
 }
